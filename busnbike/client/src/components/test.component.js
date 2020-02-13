ecf6f8406b2792dc745201635306e9740c26b87f
Jim Doyle <indellibleink@gmail.com>
2020-02-12 23:33:49 -0500

    have css working on components

--- busnbike/client/src/components/test.component.js
+++ busnbike/client/src/components/test.component.js
@@ -1,11 +1,22 @@
 // Basic React Component
+// inline: css h3 tag  normally it's background-color. React it's camelCase backgroundColor
 import React, {Component} from 'react';
 
 export default class Test extends Component {
     render() {
+        const pStyle = {
+            color: "white",
+            backgroundColor: "DodgerBlue",
+            padding: "10px",
+            fontFamily: "Arial"
+          };
         return (
             <div>
+                <h3 style={{backgroundColor: "lightblue"}}>Test Component</h3>
+                <br />
                 <p style={{color: 'red'}}>This line will display on page.</p>
+                <br />
+                <p style={pStyle}>This is a style object, simular to a style sheet in a html file.</p>
             </div>
         )
     }

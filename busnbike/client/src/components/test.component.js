a104ea9ef62d50a5831c6896eeb8b8e49b429d3d
Jim Doyle <indellibleink@gmail.com>
2020-02-13 01:56:39 -0500

    setup some CSS methods in test component

--- busnbike/client/src/components/test.component.js
+++ busnbike/client/src/components/test.component.js
@@ -1,6 +1,7 @@
 // Basic React Component
 // inline: css h3 tag  normally it's background-color. React it's camelCase backgroundColor
 import React, {Component} from 'react';
+import '../css/test.css';
 
 export default class Test extends Component {
     render() {
@@ -12,11 +13,15 @@ export default class Test extends Component {
           };
         return (
             <div>
-                <h3 style={{backgroundColor: "lightblue"}}>Test Component</h3>
+                <h3>This is from a CSS file</h3>
+                <br />
+                <p style={{backgroundColor: "lightblue"}}>camelCase inline CSS styling</p>
                 <br />
                 <p style={{color: 'red'}}>This line will display on page.</p>
                 <br />
                 <p style={pStyle}>This is a style object, simular to a style sheet in a html file.</p>
+                <br />
+                
             </div>
         )
     }

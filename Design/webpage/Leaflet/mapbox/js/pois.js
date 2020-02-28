dfd2377f29114f4bab5fef050b7c7b4cf746dfd1
Jim Doyle <indellibleink@gmail.com>
2020-02-27 21:23:08 -0500

    User can input GPS coors and display on Map

--- Design/webpage/Leaflet/mapbox/js/pois.js
+++ Design/webpage/Leaflet/mapbox/js/pois.js
@@ -2,9 +2,9 @@ function OceanMist(){
     let ocaeanMistGPS=[41.3743224,-71.5454094];
     riMap.setView(ocaeanMistGPS, 18);
     let markOceanMist = L.marker(ocaeanMistGPS)
-                          .bindPopup("<b>Ocean Mist</b><br>It's the place to <a href='https://www.oceanmist.net/'>Be</a>!!!")           
-                          .openPopup()
-                          .addTo(riMap);
+                         .bindPopup("<b>Ocean Mist</b><br>It's the place to <a href='https://www.oceanmist.net/'>Be</a>!!!")           
+                         .openPopup()
+                         .addTo(riMap);
 };
 
 function Home(){
@@ -17,15 +17,15 @@ function Home(){
 
 };
 
-function Location(lat,lng){
+function Location(){
     let locationGPS=[];
-    let lat = document.getElementsById("latitude").value;
-    let lng = document.getElementsById("longtitude").value;
-    
+    let lat = document.getElementById("latitude").value;
+    let lng = document.getElementById("longitude").value;
     locationGPS.push(lat);
     locationGPS.push(lng);
-    console.log("lat"+locationGPS[0]);
-    console.log("lng"+locationGPS[1]);
-
-
+    riMap.flyTo(locationGPS, 18);
+    let markLocation = L.marker(locationGPS)
+                        .bindPopup("Your Input: " + locationGPS.toString())
+                        .openPopup()
+                        .addTo(riMap);
 }

e5cb189dbfd26101c85f777b10164ebefdf51946
Jim Doyle <indellibleink@gmail.com>
2020-02-28 22:41:42 -0500

    designing materialize page

--- Design/webpage/Leaflet/mapbox/js/displaymap.js
+++ Design/webpage/Leaflet/mapbox/js/displaymap.js
@@ -1,4 +1,4 @@
-var mapBox = L.map('map').setView([41.8239891, -71.4128343], 13);
+var mapBox = L.map('map', {zoomControl: false}).setView([41.8239891, -71.4128343], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',

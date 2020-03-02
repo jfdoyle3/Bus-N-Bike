941e3b4380e75611c04ae83069cef479d7c63fdc
Jim Doyle <indellibleink@gmail.com>
2020-03-01 23:56:52 -0500

    setup a materualui react

---
+++ Design/material-ui/client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+import Test from "./components/test.component";
+
+function App() {
+  return (
+   
+   <Test />
+   
+    );
+}
+
+export default App;

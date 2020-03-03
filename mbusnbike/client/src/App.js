2f6233788842f9532df3d251868d4e3cc6f91f91
Jim Doyle <indellibleink@gmail.com>
2020-03-02 23:29:31 -0500

    Have a map display on a react page

--- mbusnbike/client/src/App.js
+++ mbusnbike/client/src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import {BrowserRouter as Router, Route } from "react-router-dom";
+import {Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import {Icon} from "leaflet";
+import "./App.css";
 
-
-// import Navbar from "./components/navbar.component"
-// import Sidebar from "./components/sidebar.component";
-// import Test from "./components/test.component";
-// import Page from "./components/pagelayout.component";
-import DisplayMap from "./components/map.component";
-function App() {
-  return (
-
-  <Router>
-    <DisplayMap /> 
-  </Router>
-  
-  );
+export default function App(){
+  return <Map center={[41.8239891,-71.4128343]} zoom={15}>
+    <TileLayer
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
+    zoomControl={false};
+    />
+  </Map>
 }
-
-export default App;
--- mbusnbike/client/src/components/map.component.js
+++ mbusnbike/client/src/components/map.component.js
@@ -1,47 +1,20 @@
-import React, { Component } from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import React from 'react';
+import L from "leaflet";
+import styled from 'styled-components';
 
-type State = {
-  lat: number,
-  lng: number,
-  zoom: number,
-}
 
-export default class MapTiles extends Component<{}, State> {
-  state = {
-    lat: 51.505,
-    lng: -0.09,
-    zoom: 13,
+ // L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+ //    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+ //    detectRetina: true,
+ //    maxZoom: 20,
+ //    id: 'mapbox/streets-v11',
+ //    tileSize: 512,
+ //    zoomOffset: -1,
+ //    accessToken: 'pk.eyJ1IjoiamZkb3lsZTMiLCJhIjoiY2s2bGpwdTM3MGNoNjNsbzZkaHRxZGxxbCJ9.qoUulrGgoNG2qLQENJG3IA'
+ // }).addTo(this.map);
   }
+  render(){
+    return <Wrapper width="100%" height="625px" id="map" />
 
-  render() {
-    const position = [this.state.lat, this.state.lng]
-    return (
-        <Map center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </Map>
-    )
   }
 }
-
-
-
-
-// var mapBox = L.map('map', {zoomControl: false}).setView([41.8239891, -71.4128343], 13);
-
-// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-// attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-// maxZoom: 18,
-// id: 'mapbox/streets-v11',
-// tileSize: 512,
-// zoomOffset: -1,
-// accessToken: 'pk.eyJ1IjoiamZkb3lsZTMiLCJhIjoiY2s2bGpwdTM3MGNoNjNsbzZkaHRxZGxxbCJ9.qoUulrGgoNG2qLQENJG3IA'
-// }).addTo(mapBox);

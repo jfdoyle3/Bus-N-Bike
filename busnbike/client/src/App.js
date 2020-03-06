37be037899805cb9e81e689d0ab2a0ff43df4506
Jim Doyle <indellibleink@gmail.com>
2020-03-06 02:23:58 -0500

    recreated react app and added antd css framework

--- busnbike/client/src/App.js
+++ busnbike/client/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import {BrowserRouter as Router, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import Navbar from "./components/navbar.component"
-import Sidebar from "./components/sidebar.component";
-import Test from "./components/test.component"
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-
- 
-  <Router>
-    <div className="container">
-     <Navbar />
-     <Sidebar />
-     <Test />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  </Router>
- 
   );
 }
 

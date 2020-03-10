5f483fef446e556c5253e391500145bbd1ac0e59
Jim Doyle <indellibleink@gmail.com>
2020-03-09 20:39:34 -0400

    removed commened code

--- Design/webpage/Leaflet/mapbox/js/findlocation.js
+++ Design/webpage/Leaflet/mapbox/js/findlocation.js
@@ -1,15 +1,10 @@
 const location="North Providence";
 const url="https://api.mapbox.com/geocoding/v5/mapbox.places/"+location+".json?access_token=pk.eyJ1IjoiamZkb3lsZTMiLCJhIjoiY2s2bGpwdTM3MGNoNjNsbzZkaHRxZGxxbCJ9.qoUulrGgoNG2qLQENJG3IA";
-const data="AddressData(data)";
 
-//FindByAddress(url);
-//let data=FindByAddress(url);
-//var dInfo=JSON.parse(data);
-//console.log(dInfo);
-Type(data);
+
+
 FindByAddress(url);
-//var getAResp=AddressData(data);
-//console.log(getAResp);
+
 
 function FindByAddress(url) {
 
@@ -18,18 +13,16 @@ const https=require ('https');
 https.get(url, res => {
   let body = "";
 
-  // A chunk of data has been recieved.
+
   res.on("data", data => {
     body += data;
   });
 
-  // The whole response has been received. Print out the result.
   res.on('end', () => {
-    //body=JSON.parse(body);  
-    // jsonData=JSON.stringify(body); 
-    //console.log(body);
-   // console.log(JSON.parse(jsonData));
-  return body;
+    body=JSON.parse(body);  
+
+    console.log(body);
+  
   });
 
 }).on("error", (err) => {
@@ -38,9 +31,7 @@ https.get(url, res => {
 
 };
 
-function AddressData(data){
-    var returnThis="This is the return";
-    var info=JSON.parse(data);
+function AddressData(){
+    let info="This is the return";
   console.log(info);
- return returnThis;
 }

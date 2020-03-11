f2c0991d23e8dee6fba4d790cdadc4079fde3fa8
Jim Doyle <indellibleink@gmail.com>
2020-03-10 21:06:37 -0400

    removed file

--- busnbike/server.js
+++ busnbike/server.js
@@ -4,9 +4,6 @@ const bus=require('./routes/bus.route');
 
 server.use("/bus",bus);       
 
-
-
-//Launch listening server on port 5000
 server.listen(5000, () =>{
   console.log('localhost:5000');
 });

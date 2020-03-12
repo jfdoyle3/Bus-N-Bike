f2eb252e34bfd3532ac475e6f9b5a0f4356c8658
Jim Doyle <indellibleink@gmail.com>
2020-03-11 20:42:23 -0400

    remade react app

--- busnbike/server.js
+++ busnbike/server.js
@@ -1,18 +1,18 @@
-const express = require('express')
+const express = require('express'),
       server=express (),
       cors=require('cors'),
       mongoose=require('mongoose'),
       colors=require('colors'),
-      path=require('path');
+      path=require('path'),
 
+      home=require('./routes/home.route'), 
+      bus=require('./routes/bus.route'),
+      location=require('./routes/geolocation.route');
 
       server.use(cors());
       server.use(express.json());
-//Routes
-const bus=require('./routes/bus.route'),
-      location=require('./routes/geolocation.route');
 
-
+server.use("/home",home); 
 server.use("/bus",bus); 
 server.use("/location",location);     
 

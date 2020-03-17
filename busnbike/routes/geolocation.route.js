11e350fc65c7ae8060775213beca46f14c83b2cf
Jim Doyle <indellibleink@gmail.com>
2020-03-17 09:16:40 -0400

    Got a params to work on a route

--- busnbike/routes/geolocation.route.js
+++ busnbike/routes/geolocation.route.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 
-router.get("/", (req, res)=>{  
-  res.json({message: 'geolocation'});
+router.get("/", (req, res, next)=>{
+   res.send('GET /');
+  // res.json({message: '/'});
+   console.log('/');
+   next();
 });
 
-router.get("/place", (req,res)=>{
-  res.json({message: 'loc/places'});
+router.get("/:place", (req,res)=>{
+  //var geoLocate=JSON.stringify(req.query.place);
+  console.log(req.params.place);
+ // res.json({message: 'geoLocate'});
+  res.send('Response send to client= '+req.params.place);
 });
 
 
